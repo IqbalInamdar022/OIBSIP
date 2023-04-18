bdc9e7978e5d4f1081bbef32320655ea1e0fcfaf
IqbalInamdar022 <127484329+IqbalInamdar022@users.noreply.github.com>
2023-04-18 14:51:49 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,69 @@
+// getting all required elements
+const inputBox = document.querySelector(".select input");
+const addBtn = document.querySelector(".select button");
+const todoList = document.querySelector(".todoList");
+const deleteAllBtn = document.querySelector(".footer button");
+
+// onkeyup event
+inputBox.onkeyup = () => {
+    let userEnteredValue = inputBox.value;
+    if (userEnteredValue.trim() != 0) {
+        addBtn.classList.add("active");
+    } else {
+        addBtn.classList.remove("active");
+    }
+}
+
+showTasks();
+
+addBtn.onclick = () => {
+    let userEnteredValue = inputBox.value;
+    let getLocalStorageData = localStorage.getItem("New Todo");
+    if (getLocalStorageData == null) {
+        listArray = [];
+    } else {
+        listArray = JSON.parse(getLocalStorageData);
+    }
+    listArray.push(userEnteredValue);
+    localStorage.setItem("New Todo", JSON.stringify(listArray));
+    showTasks();
+    addBtn.classList.remove("active");
+}
+
+function showTasks() {
+    let getLocalStorageData = localStorage.getItem("New Todo");
+    if (getLocalStorageData == null) {
+        listArray = [];
+    } else {
+        listArray = JSON.parse(getLocalStorageData);
+    }
+    if (listArray.length > 0) {
+        deleteAllBtn.classList.add("active");
+    } else {
+        deleteAllBtn.classList.remove("active");
+    }
+    let newLiTag = "";
+    listArray.forEach((element, index) => {
+        newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
+    });
+
+    todoList.innerHTML = newLiTag;
+    inputBox.value = "";
+}
+
+// delete task function
+function deleteTask(index) {
+    let getLocalStorageData = localStorage.getItem("New Todo");
+    listArray = JSON.parse(getLocalStorageData);
+    listArray.splice(index, 1);
+    localStorage.setItem("New Todo", JSON.stringify(listArray));
+    showTasks();
+
+}
+
+// delete all  function
+deleteAllBtn.onclick = () => {
+    listArray = [];
+    localStorage.setItem("New Todo", JSON.stringify(listArray));
+    showTasks();
+}
